6333f7b6a1dfff01446b24c47628c6bed1d1c7ae
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2019-08-05 18:02:40 +0530

    update

--- Canvas-Examples/First-Canvas-Example/canvas.js
+++ Canvas-Examples/First-Canvas-Example/canvas.js
@@ -7,5 +7,14 @@ var context = canvas.getContext('2d');
 
 // context.fillRect(x,y,width,height);
 context.fillRect(100, 100, 100, 100);
-context.fillRect(300, 100, 100, 100);
+context.fillRect(300, 400, 100, 100);
 context.fillRect(500, 100, 100, 100);
+
+
+//lines
+context.beginPath();
+// context.moveTo(x,y);
+context.moveTo(50,300);
+context.lineTo(300,100);
+context.lineTo(400,300);
+context.stroke();
