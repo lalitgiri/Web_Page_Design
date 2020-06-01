1cb59d44c0a4a5e646cd70f382a231c864fa3a19
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2020-06-01 23:44:57 +0530

    conditional scss in react

---
+++ conditional-scss-in-react/conditional-scss-in-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div class="header">
+      <h1>
+        hello
+      </h1>
+      <label for="" class='label'>
+        label
+       </label>
+    </div>
+  );
+}
+
+export default App;
